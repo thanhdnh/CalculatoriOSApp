651d677bc4d7834b389b4c31a4722231eeec8e24
thanhdnh <myhoangthanh@yahoo.com>
2022-02-06 18:51:55 +0700

    Initial Commit

---
+++ CalculatoriOSApp/CalculatoriOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatoriOSAppApp.swift
+//  CalculatoriOSApp
+//
+//  Created by Hoang Thanh on 06/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatoriOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
